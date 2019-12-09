20438ef06437edd49ed4e5bc227f3b74978285d1
GDalforno <56834802+GDalforno@users.noreply.github.com>
2019-12-09 11:14:02 -0300

    Update ridge.py

--- paje/ml/element/modelling/supervised/regressor/ridge.py
+++ paje/ml/element/modelling/supervised/regressor/ridge.py
@@ -9,7 +9,7 @@ from paje.ml.element.modelling.supervised.regressor.regressor import Regressor
 class RIDGE(Regressor):
     def __init__(self, config, **kwargs):
         super().__init__(config, **kwargs)
-        self.model = Ridge()
+        self.model = Ridge(**self.param())
 
     @classmethod
     def cs_impl(cls):
